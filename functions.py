5ca23e5c0f9cbf9bf7045db79b53fab2ad213cd8
DShymariev <116166312+DShymariev@users.noreply.github.com>
2022-10-25 22:36:01 +0300

    Add window to calculator

--- functions.py
+++ functions.py
@@ -51,3 +51,23 @@ def k3k():
     k3k1 = x ** (1 / 3)
     label.config(text=k3k1)
 
+
+Label(win, text="Введіть перше число ", font=('Calibri 17')).pack()
+a = Entry(win, width=35)
+a.pack()
+Label(win, text="Введіть друге число ", font=('Calibri 17')).pack()
+b = Entry(win, width=35)
+b.pack()
+
+label = Label(win, text="Результат ", font=('Calibri 17'))
+label.pack(pady=20)
+
+Button(win, text="Додавання чисел", command=sum,bg="sky blue").pack()
+Button(win, text="Віднімання чисел", command=riz,bg="sky blue").pack()
+Button(win, text="Множення чисел", command=mno,bg="sky blue").pack()
+Button(win, text="Ділення числа", command=dil,bg="sky blue").pack()
+Button(win, text="Зведення числа в ступінь", command=zve,bg="sky blue").pack()
+Button(win, text="Квадратний корінь першого числа", command=k2k,bg="sky blue").pack()
+Button(win, text="Кубічний корінь першого числа", command=k3k,bg="sky blue").pack()
+
+win.mainloop()
